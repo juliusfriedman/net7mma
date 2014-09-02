43b712172ddd00599002395a5bfe05b2b0df751c
Julius R Friedman <juliusfriedman@gmail.com>
2014-09-02 17:53:31 +0000

    RtpClient - ReadRFC2326FrameHeader logic fixed, now only iterates until the `BigEndianFrameControl` byte is found, for non framed messages this allows them to be interleaved completely resulting in less Interleaved events as well as to fix potential framing sync errors.
    RtpFrame - now reports a new exception for when a packet is already contained.

--- Rtp/RtpFrame.cs
+++ Rtp/RtpFrame.cs
@@ -226,7 +226,8 @@ namespace Media.Rtp
             
             //E.g with respect to the above checks and finally the check below 
             //Which determineres if the RtpFrame is already complete that the packet will not be added to this frame.
-            if (Complete || Contains(packet)) throw new InvalidOperationException("Complete frame cannot have additional packets added");
+            if (Complete) throw new InvalidOperationException("Complete frame cannot have additional packets added");
+            else if (Contains(packet)) throw new InvalidOperationException("Packet already added to frame.");
 
             //Add the packet to the SortedList which will not throw any exception if the RtpPacket added already contains a value.
             m_Packets.Add(packet.SequenceNumber, packet);
