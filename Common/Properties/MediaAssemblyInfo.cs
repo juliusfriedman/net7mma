f614e57d9643147ae0131bf606ace2395d619374
juliusfriedman_cp <SND\juliusfriedman_cp>
2014-11-04 23:24:28 +0000

    Shared AssemblyInfo across projects. Renamed Media to Media.Rtmp

---
+++ Common/Properties/MediaAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("v//")]
+[assembly: AssemblyProduct("Media")]
+[assembly: AssemblyCopyright("Copyright © Julius R. Friedman")]
+[assembly: AssemblyTrademark("v//")]
+[assembly: AssemblyCulture("")]
---
+++ Common/Properties/MediaCLSInternalVisibleToTestAttribute.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Media.Tests")]
