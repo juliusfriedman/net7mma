94efd5f2424b1697454e88dbe49f1ea68b44ee8f
Julius R Friedman <juliusfriedman@gmail.com>
2014-11-21 04:38:39 +0000

    *Stable Nightly Container Release*
    Removed Codecs.Video.Mpeg1 and Codecs.Video.Mpeg2 Merged into Codecs.Video.Mpeg for simplicity.
    
    Added Codecs.Audio.Mpeg to encapsulate MP3
    
    RtspServer Sources is now MediaTypes.
    
    Containers.Mpeg - ElementaryStreamReader is now PacketizedElementaryStreamReader.
    
    Added StartCodes to encapsulate common Mpeg Start Codes which used in all Mpeg types.
    
    Added StreamTypes - Moved from Codecs.Video.Mpeg4 because it belongs to the container not the codec.
    
    ProgramStreamReader - Now reads Pack Header.
    
    PacketizedElementaryStreamReader - Added, reads PES Packets.
    
    Program updated for testing.

--- Codecs/Video/Mpeg/Properties/AssemblyInfo.cs
+++ Codecs/Video/Mpeg/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Media.Codecs.Video.Mpeg")]
-[assembly: AssemblyDescription("Implementations of various technologies related to the encoding and decoding of MPEG Video")]
+[assembly: AssemblyTitle("Media.Codecs.Video.Mpeg1")]
+[assembly: AssemblyDescription("Implementations of various technologies related to the encoding and decoding of MPEG 4 Video")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
