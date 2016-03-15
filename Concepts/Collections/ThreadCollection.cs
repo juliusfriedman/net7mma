b25b181c4fd278432799497538abff9ff235efa1
Julius R Friedman <juliusfriedman@gmail.com>
2016-03-15 15:23:20 +0000

    *Formats Beta*
    RtspClient - Fixed bug when AutomaticallyReconnect was false and the client still tried to reconnect.
    
    Removed Common.SocketCollection, Common.ThreadCollection for now.
    
    Added notes for 17623

--- Concepts/Collections/ThreadCollection.cs
+++ Concepts/Collections/ThreadCollection.cs
@@ -7,7 +7,82 @@ using System.Threading.Tasks;
 namespace Media.Concepts.Collections
 {
     //A collection which caters to threads
-    public class ThreadCollection
+    //A collection which caters to threads
+    public class ThreadCollection : System.Collections.ICollection,
+        ICollection<System.Threading.Thread>,
+        Media.Common.IThreadReference
     {
+        IEnumerable<System.Threading.Thread> Media.Common.IThreadReference.GetReferencedThreads()
+        {
+            using (var e = ((ICollection<System.Threading.Thread>)this).GetEnumerator())
+            {
+                while (e.MoveNext()) yield return e.Current;
+            }
+        }
+
+        void ICollection<System.Threading.Thread>.Add(System.Threading.Thread item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<System.Threading.Thread>.Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ICollection<System.Threading.Thread>.Contains(System.Threading.Thread item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<System.Threading.Thread>.CopyTo(System.Threading.Thread[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        int ICollection<System.Threading.Thread>.Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        bool ICollection<System.Threading.Thread>.IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        bool ICollection<System.Threading.Thread>.Remove(System.Threading.Thread item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator<System.Threading.Thread> IEnumerable<System.Threading.Thread>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        void System.Collections.ICollection.CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        int System.Collections.ICollection.Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        bool System.Collections.ICollection.IsSynchronized
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        object System.Collections.ICollection.SyncRoot
+        {
+            get { throw new NotImplementedException(); }
+        }
     }
 }
